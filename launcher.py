deb6a54ea8ad86610e694ad51431bd1f2b4e0bf3
Carlos Andrés Pupiales Córdoba <capc3101@gmail.com>
2025-08-15 18:29:53 -0500

    se mejora archivo de config

--- launcher.py
+++
@@ -1,7 +0,0 @@
-# launcher.py
-import typer
-from pdf_ocr_stamper.cli import main as app_main
-
-if __name__ == "__main__":
-    # esto emula `python -m pdf_ocr_stamper.cli`
-    typer.run(app_main)
